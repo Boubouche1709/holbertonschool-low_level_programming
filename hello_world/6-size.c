01453258147c2bf2a8023eaa7dc078d94f324b99
Boubouche1709 <hadjarflorian@gmail.com>
2025-02-18 12:38:28 +0100

    allSize

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,10 +2,15 @@
 
 /**
  * main - Start of the code
+ * Description some sizes
  * Return: always 0 (success)
  */
 int main(void)
 {
+	printf("Size of a char : %zu byte(s)\n", sizeof(char));
 	printf("Size of an int : %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int : %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int : %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float : %zu byte(s)\n", sizeof(float));
 	return (0);
 }
