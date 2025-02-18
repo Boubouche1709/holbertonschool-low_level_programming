e5196e7e328aa098771ab7bf48bf08dfeae407a0
Boubouche1709 <hadjarflorian@gmail.com>
2025-02-18 12:23:09 +0100

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Start of the code
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("La taille de int est :%zu octets\n", sizeof(int));
+	return (0);
+}
