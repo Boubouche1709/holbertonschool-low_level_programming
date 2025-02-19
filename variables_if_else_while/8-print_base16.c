34a587e92df8b67ff800c50e0f77fbdb7fa9ec50
Boubouche1709 <hadjarflorian@gmail.com>
2025-02-19 11:02:21 +0100

    printBase16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the code
+ *
+ * Description: Prints all single digit numbers of base 16 starting from 0 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+putchar(num + '0');
+num++;
+}
+num = 0;
+while (num <= 5)
+{
+putchar(num + 'a');
+num++;
+}
+putchar('\n');
+return (0);
+}
