6b8338ad677f8ddc389e63011c8ab50e6c98ab73
Boubouche1709 <hadjarflorian@gmail.com>
2025-02-20 09:30:37 +0100

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print exactly _putchar with putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
