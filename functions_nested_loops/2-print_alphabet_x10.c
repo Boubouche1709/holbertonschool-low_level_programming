a5ccba8956c844e84412ab8e4b44faa8061e2a2a
Boubouche1709 <hadjarflorian@gmail.com>
2025-02-20 13:38:11 +0100

    printAlphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void - check the code
+ *
+ * Description: Print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{	
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+int i, j;
+for (j = 0; j < 10; j++)
+{	
+for (i = 0; alphabet[i] != '\0'; i++)
+{
+_putchar(alphabet[i]);
+}
+_putchar('\n');
+}
+}
