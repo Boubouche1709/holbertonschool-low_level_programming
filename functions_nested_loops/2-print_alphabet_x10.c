9ebab19f0d70a19445d7b2deed6c6612b1cdc732
Boubouche1709 <hadjarflorian@gmail.com>
2025-02-20 14:05:05 +0100

    printAlphabetx10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@
  */
 
 void print_alphabet_x10(void)
-{	
+{
 char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 int i, j;
 for (j = 0; j < 10; j++)
-{	
+{
 for (i = 0; alphabet[i] != '\0'; i++)
 {
 _putchar(alphabet[i]);
