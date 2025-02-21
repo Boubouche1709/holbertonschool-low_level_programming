bf1cefa959e5d981aa87a7daf35e79c8cecd2d31
Boubouche1709 <hadjarflorian@gmail.com>
2025-02-21 14:35:31 +0100

    printFromNto98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: print all numbers from n to 98
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+print_to_98(0);
+print_to_98(111);
+print_to_98(98);
+print_to_98(-10);
+return (0);
+}
