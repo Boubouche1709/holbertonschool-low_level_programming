7e2fd38392447543f778773afca57f3aa6d66efa
Boubouche1709 <hadjarflorian@gmail.com>
2025-02-24 09:49:20 +0100

    isUpper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Entry of the code
+ *
+ * @c: Character to check
+ *
+ * Return: 1 for success, 0 otherwise
+ */
+
+int _isupper(int c);
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
