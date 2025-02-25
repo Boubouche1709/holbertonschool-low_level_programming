1045d78f0be416e894639ea984e0e2c19901b49e
Boubouche1709 <hadjarflorian@gmail.com>
2025-02-25 09:37:08 +0100

    multipleTwoInteger

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiple two integers
+ *
+ * @a: First integer
+ *
+ * @b: Second integer
+ *
+ * Return: The multiple of a and b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
