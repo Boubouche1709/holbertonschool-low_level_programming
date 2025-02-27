84b30433d7a6d68ccb1264ad31f55aa1f6121669
Boubouche1709 <hadjarflorian@gmail.com>
2025-02-27 10:26:53 +0100

    resetTo98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,36 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
-/** 
- * main - Entry point of the code
+/**
+ * reset_to_98 - Update the value
  *
- * Description:
- *
- * Return: Alaways 0 (Success)
+ * @n: pointer to an int
  */
-
-int main(void)
+                                                                                            void reset_to_98(int *n)
 {
-int n;
-n = 402;
-_putchar('n');
-_putchar('=');
-if (n >= 100)
-{
-_putchar((n / 100) + '0');
-_putchar(((n /10) % 10) + '0');
-}
-else if (n >= 10)
-{
-_putchar((n / 10) + '0');
-}
-_putchar((n % 10) + '0');
-_putchar('\n');
-reset_to_98(&n);
-_putchar('n');
-_putchar('=');
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-_putchar('\n');
-return (0);
+*n = 98;
 }
