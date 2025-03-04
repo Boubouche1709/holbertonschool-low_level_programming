368e47426b66809ee8077e4254d0c1fa0633a863
Boubouche1709 <hadjarflorian@gmail.com>
2025-03-04 16:18:48 +0100

    printDiagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 
 /**
+ * print_number - print an int
+ * @n: int to print
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+putchar('-');
+n = -n;
+}
+if (n / 10)
+{
+print_number(n / 10);
+}
+putchar(n % 10 + '0');
+}
+
+/**
  * print_diagsums - print the sum of two diagonal
  * @a: array of the matrix
  * @size: size of the square
@@ -10,14 +28,14 @@ void print_diagsums(int *a, int size)
 {
 int i;
 int sum1 = 0, sum2 = 0;
-char buffer[50];
-int index = 0;
 for (i = 0; i < size; i++)
 {
 sum1 += a[i * size + i];
 sum2 += a[i * size + (size - i - 1)];
 }
-index += sprintf(buffer, "%d, %d", sum1, sum2);
-buffer[index] = '\0';
-puts(buffer);
+print_number(sum1);
+putchar(',');
+putchar(' ');
+print_number(sum2);
+putchar('\n');
 }
