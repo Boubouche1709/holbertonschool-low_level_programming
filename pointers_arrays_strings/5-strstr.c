1ec8394c59568a54f52872aa55f8817df59ef5a5
Boubouche1709 <hadjarflorian@gmail.com>
2025-03-04 11:41:51 +0100

    strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - find a substring
+ *
+ * @haystack: principal str
+ *
+ * @needle: substring to find
+ *
+ * Return: to a pointer or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+while (*haystack)
+{
+char *h = haystack;
+char *n = needle;
+while (*n && (*h == *n))
+{
+h++;
+n++;
+}
+if (*n == '\0')
+return (haystack);
+haystack++;
+}
+return (NULL);
