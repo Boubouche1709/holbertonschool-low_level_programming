89a6acbf42da89e8fd48aa3d5215707c612c493a
Boubouche1709 <hadjarflorian@gmail.com>
2025-03-04 11:23:34 +0100

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - search char in a str present in other str
+ * @s: str to search
+ *
+ * @accept: char to search
+ *
+ * Return: pointer to the first char or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+return (s + i);
+}
+}
+}
+return (NULL);
+}
