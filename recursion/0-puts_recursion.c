79daff31fec3c3ddfb91008c197f7fde80d0c299
Boubouche1709 <hadjarflorian@gmail.com>
2025-03-06 08:43:58 +0100

    putsRecursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0
+ * _puts_recursion - print a string
+ * @s: pointer to string
  */
-
-int main(void)
+void _puts_recursion(char *s)
 {
-char *str = "Holberton School";
-_puts_recursion(str);
-return (0);
+if (*s == '\0')
+{
+putchar('\n');
+return;
+}
+putchar(*s);
+_puts_recursion(s + 1);
 }
