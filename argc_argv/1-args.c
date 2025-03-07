be9990ef160f3cf545519ef1b20e7e2ab9388b0b
Boubouche1709 <hadjarflorian@gmail.com>
2025-03-07 10:35:58 +0100

    arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
