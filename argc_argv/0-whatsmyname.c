ab2562c21330edbb4283a5c2646eb18b3c5a558e
Boubouche1709 <hadjarflorian@gmail.com>
2025-03-07 09:33:02 +0100

    whatsMyName

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,21 +1,21 @@
 #include "main.h"
 #include <stdio.h>
- /**
-  * main - entry point of the program
-  * @argc: number of argument
-  * @argv: array of argument
-  * Description: print the name of the program
-  * Return: always 0 (Success)
-  */
+/**
+* main - entry point of the program
+* @argc: number of argument
+* @argv: array of argument
+*
+* Return: always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
-int i = 0;
-(void)argc;
-while (argv[0][i] != '\0')
-{
-putchar(argv[0][i]);
-i++;
-}
-putchar('\n');
-return (0);
+        int i = 0;
+        (void)argc;
+        while (argv[0][i] != '\0')
+        {
+                putchar(argv[0][i]);
+                i++;
+        }
+        putchar('\n');
+        return (0);
 }
