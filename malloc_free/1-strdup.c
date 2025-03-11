8b19108fecaf1782cbc335c5ae8db21eb18f996d
Boubouche1709 <hadjarflorian@gmail.com>
2025-03-11 09:18:55 +0100

    duplicateString

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - return a pointer to a new space
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicate string or NULL
+ */
+char *_strdup(char *str)
+{
+char *dup_str;
+unsigned int i, length = 0;
+while (str[length] != '\0')
+length++;
+dup_str = malloc((length + 1) * sizeof(char));
+if (dup_str == NULL)
+return (NULL);
+for (i = 0; i < length; i++)
+dup_str[i] = str[i];
+dup_str[length] = '\0';
+return (dup_str);
+}
