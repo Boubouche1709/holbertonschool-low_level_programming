571916d7527498e4686ed6fe1cc4a255719df36f
Boubouche1709 <hadjarflorian@gmail.com>
2025-03-12 08:36:16 +0100

    main.h

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 int _putchar(char c);
+void print_grid(int **grid, int width, int height);
 #endif /*MAIN_H*/
