110a9208978a79bd59fb12304b0ac049cff8b6b1
Boubouche1709 <hadjarflorian@gmail.com>
2025-03-13 11:40:43 +0100

    main.h

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include "dog.h"
-dog_t *create_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* MAIN_H */
