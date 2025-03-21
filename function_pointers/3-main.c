ebb6e389e02e3fc49bf703bcf16b8cbb99c09a5a
Boubouche1709 <hadjarflorian@gmail.com>
2025-03-21 10:27:10 +0100

    codeTest

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -21,15 +21,10 @@ exit(98);
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
 operator = get_op_func(argv[2]);
-if (operator == NULL)
+if (operator == NULL || argv[2][1] != '\0')
 {
 printf("Error\n");
 exit(99);
-}
-if (*argv[2] == '/' || (*argv[2] == '%' && num2 == 0))
-{
-printf("Error\n");
-exit(100);
 }
 result = operator(num1, num2);
 printf("%d\n", result);
