45f2f5fa7cc354c20d224ed943bba6a37cfddc46
Boubouche1709 <hadjarflorian@gmail.com>
2025-05-07 08:15:40 +0000

    Check and class the last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /*
- * main - entrypoint of the code
- * Return: alaways 0 (Success)
+ * main - entry point of the code
+ * Return: always 0 (Success)
  */
 int main(void)
 {
@@ -13,17 +13,17 @@ int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
-if (n <= 5 % 10)
+if (last_digit > 5 && last_digit != 0)
 {
 printf("Last digit of %d is %d and is greater than 5 \n", n, last_digit);
 }
-else if (n >= 6 % 10 && n != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, last_digit);
-}
-else
+else if (last_digit == 0)
 {
 printf("Last digit of %d is %d and is 0 \n", n, last_digit);
+}
+else if (last_digit < 6 && last_digit != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, last_digit);
 }
 return (0);
 }
