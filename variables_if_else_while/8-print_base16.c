d70c7f6f356bcb6ca7239b69caf3ad5d00b381a4
Boubouche1709 <hadjarflorian@gmail.com>
2025-06-05 14:51:47 +0200

    Print number in base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -entry point of the code
+ * Return: always 0 success
+ */
+int main(void)
+{
+char c, d;
+for (c = 0; c <= 9; c++)
+{
+putchar(c + '0');
+}
+for (d = 'a'; d <= 'f'; d++)
+{
+putchar(d);
+}
+putchar('\n');
+return (0);
+}
