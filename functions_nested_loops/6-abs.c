b092ecd3081387d12ed6225b42d44eecd7ab281f
Boubouche1709 <hadjarflorian@gmail.com>
2025-06-09 13:25:22 +0200

    Computes the absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * _abs - print the absolute value
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+if (i > 0)
+{
+return (abs(i));
+}
+else if (i < 0)
+{
+return (abs(i));
+}
+else
+{
+return (abs(i));
+}
+}
