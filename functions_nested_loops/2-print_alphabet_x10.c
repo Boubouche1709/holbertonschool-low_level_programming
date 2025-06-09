a8d2a02259246e3fac845da87667258f1a75746a
Boubouche1709 <hadjarflorian@gmail.com>
2025-06-09 10:00:24 +0200

    Print the alphabet ten times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet_x10 - function to print the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
