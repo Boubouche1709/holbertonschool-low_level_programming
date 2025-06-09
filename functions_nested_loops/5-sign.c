afb4c72a450a561379c4fbe20b37733ade7063af
Boubouche1709 <hadjarflorian@gmail.com>
2025-06-09 12:18:49 +0200

    Check if the digit is grater, lower or equal to 0

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_sign - print the singn before a digit
+ * @n: integer to check
+ * Return: 1 if is greater than 0, 0 if is 0, -1 otherwise
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
