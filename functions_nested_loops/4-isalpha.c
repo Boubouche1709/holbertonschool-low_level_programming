750c31f1174b23c58419fd2b9316543fd2e666b5
Boubouche1709 <hadjarflorian@gmail.com>
2025-06-10 09:23:31 +0200

    Check if is an alphabet character

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
 #include "main.h"
-#include <unistd.h>
-#include <ctype.h>
+
 /**
  * _isalpha - check if the chrracter is a lowercase
  * @c: character to check
@@ -9,7 +7,11 @@
  */
 int _isalpha(int c)
 {
-if (isalpha(c))
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
