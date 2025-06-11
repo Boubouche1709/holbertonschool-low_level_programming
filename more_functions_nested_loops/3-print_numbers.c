f20b09ac0ce99ccdfdd7790d0e78edcd611f66a8
Boubouche1709 <hadjarflorian@gmail.com>
2025-06-11 10:49:00 +0200

    Print numbers from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print the alphabet
+ */
+
+void print_numbers(void)
+{
+char c;
+for (c = 0; c <= 9; c++)
+{
+_putchar(c + '0');
+}
+_putchar('\n');
+}
