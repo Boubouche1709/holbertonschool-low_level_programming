47554fffd5b0fc92fbb039740626a4e05d9ffc42
Boubouche1709 <hadjarflorian@gmail.com>
2025-06-11 15:51:51 +0200

    Print nine times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,13 +10,21 @@ int j;
 int result;
 for(i = 0; i <= 9; i++)
 {
-for(j = 0; j <= 9; j++)
-{
-	result = i * j;
-	_putchar(result);
-	_putchar(',');
-	_putchar(' ');
+	for(j = 0; j <= 9; j++)
+	{
+		result = i * j;
+		if (j != 0)
+		{
+		_putchar(',');
+        	_putchar(' ');
+		}
+		if (result < 10 && j != 0)
+		_putchar(' ');
+		if (result >= 10)
+	_putchar((result / 10) + '0');
+	_putchar((result % 10) + '0');
+	}
+
+	_putchar('\n');
 }
-}
-_putchar('\n');
 }
