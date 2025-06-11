7ea4a537f4d01374daa346a77b70a142947a0dbe
Boubouche1709 <hadjarflorian@gmail.com>
2025-06-11 09:58:54 +0200

    Print a list of number from or to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_to_98 - print a list of number
+ * @n: int to print
+ */
+
+void print_to_98(int n)
+{
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+printf("%d\n", n);
+}
