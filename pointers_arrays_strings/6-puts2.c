7d20e64b7e591c37fb8ca96cdcf5710a66f2e6fe
Boubouche1709 <hadjarflorian@gmail.com>
2025-06-24 15:50:05 +0200

    test

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts2 - print a string
+ * @str: string to print
+ */
+
+void puts2(char *str)
+{
+int n;
+for (n = 0; str[n] != '\0'; n += 2)
+{
+_putchar(str[n]);
+}
+{
+_putchar('\n');
+}
+}
