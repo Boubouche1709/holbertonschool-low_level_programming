f7e572eba4a5b17535b4d1c0f9d02be68bd183a6
Boubouche1709 <hadjarflorian@gmail.com>
2025-06-25 16:31:03 +0200

    replicate the function atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *_atoi - convert a string to an integer
+ @s: string to convert
+ */
+
+int _atoi(char *s)
+{
+unsigned int result = 0;
+int sign = 1;
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+{
+sign *= -1;
+}
+else if (s[i] >= '0' && s[i] <= '9')
+{
+result = result * 10 + (s[i] - '0');
+}
+else if (result > 0)
+{
+break;
+}
+i++;
+}
+return (result * sign);
+}
