01f34c2a89c6a01c0900442d4203af5da1e4a54b
Boubouche1709 <hadjarflorian@gmail.com>
2025-07-16 10:26:17 +0200

    File to choose the good operator

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "calc.h"
+#include <stdio.h>
+
+/**
+* get_op_func - choose the good operator
+* @s: pointer to the operator
+* Return: pointer to th function or NULL
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+};
+
+int i = 0;
+
+while (ops[i].op)
+	if (*(ops[i].op) == *s && s[1] == '\0')
+		return (ops[i].f), NULL;
+return (NULL);
+}
