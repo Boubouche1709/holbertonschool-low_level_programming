3555df2f59dca41537f443e583778701dcc16874
Boubouche1709 <hadjarflorian@gmail.com>
2025-07-16 09:58:41 +0200

    File countains all operaturs functions

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "calc.h"
+#include <stdio.h>
+
+/**
+* op_add - add two int
+* @a: first int
+* @b: second int
+* Return: result
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - sub two int
+* @a: first int
+* @b: second int
+* Return: result
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - sub two int
+* @a: first int
+* @b: second int
+* Return: result
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - sub two int
+* @a: first int
+* @b: second int
+* Return: result
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - sub two int
+* @a: first int
+* @b: second int
+* Return: result
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
