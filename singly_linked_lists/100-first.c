f6af4fd52718c483ce64e6c1d370d9c73e469d8f
Boubouche1709 <hadjarflorian@gmail.com>
2025-07-29 08:23:12 +0200

    Write a function that prints a text before the main function is executed

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+* intro - function to print before main function
+*/
+void intro(void) __attribute__((constructor));
+void intro(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
