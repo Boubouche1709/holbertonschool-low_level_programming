2d297f169e8a4ed83fb63285d2ed2d93083923bc
Boubouche1709 <hadjarflorian@gmail.com>
2025-07-31 10:30:58 +0200

    Fix betty style

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,36 +1,41 @@
 #include "lists.h"
 
-
+/**
+* delete_dnodeint_at_index - Deletes the node at a given index in list
+* @head: Double pointer to the head of the list.
+* @index: Index of the node to be deleted (starts at 0).
+* Return: 1 if it succeeded, -1 if it failed.
+*/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 dlistint_t *current;
 unsigned int i;
 if (head == NULL || *head == NULL)
-	return (-1);
+return (-1);
 
 current = *head;
 if (index == 0)
-	{
-        	*head = current->next;
-        	if (*head != NULL)
-		{
-            		(*head)->prev = NULL;
-        		free(current);
-		}
-        	return (1);
-    	}
+{
+*head = current->next;
+if (*head != NULL)
+{
+(*head)->prev = NULL;
+free(current);
+}
+return (1);
+}
 
 for (i = 0; current != NULL && i < index; i++)
-        current = current->next;
+current = current->next;
 
 if (current == NULL)
-        return (-1);
+return (-1);
 
 if (current->prev)
-        current->prev->next = current->next;
+current->prev->next = current->next;
 
 if (current->next)
-        current->next->prev = current->prev;
+current->next->prev = current->prev;
 
 free(current);
 return (1);
